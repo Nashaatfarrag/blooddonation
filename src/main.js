e7a7672e55fefc40e3fdf9bebdcbd3e1a9592334
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2020-06-26 19:10:24 +0200

    bug fixes

--- src/main.js
+++ src/main.js
@@ -3,26 +3,29 @@ import App from "./App.vue";
 import router from "./router";
 import VAnimateCss from "v-animate-css";
 import VueSweetalert2 from "vue-sweetalert2";
-import Clipboard from 'v-clipboard'
-
-Vue.use(Clipboard)
+import Clipboard from "v-clipboard";
+import BootstrapVue from "bootstrap-vue";
+import VueAnalytics from "vue-analytics";
 
 // If you don't need the styles, do not connect
 import "sweetalert2/dist/sweetalert2.min.css";
 
-Vue.use(VueSweetalert2);
-
 import {
   FontAwesomeIcon,
   FontAwesomeLayers,
-  FontAwesomeLayersText
+  FontAwesomeLayersText,
 } from "@fortawesome/vue-fontawesome";
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.component("font-awesome-layers", FontAwesomeLayers);
 Vue.component("font-awesome-layers-text", FontAwesomeLayersText);
 
-import BootstrapVue from "bootstrap-vue";
+Vue.use(Clipboard);
+Vue.use(VueSweetalert2);
+Vue.use(VueAnalytics, {
+  id: "UA-146314933-1",
+  router,
+});
 
 Vue.use(BootstrapVue);
 Vue.use(VAnimateCss);
@@ -30,5 +33,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
