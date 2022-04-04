1a2c65a642ec44019378a19d67e4b0d9db13e75a
Mohamed Nashaat <nashaatfarrag@gmail.com>
2022-04-04 19:20:45 +0200

    add new analytics page

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import VAnimateCss from "v-animate-css";
 import VueSweetalert2 from "vue-sweetalert2";
 import Clipboard from "v-clipboard";
 // import BootstrapVue from "bootstrap-vue";
-import VueAnalytics from "vue-analytics";
 
 // If you don't need the styles, do not connect
 import "sweetalert2/dist/sweetalert2.min.css";
@@ -17,6 +16,7 @@ import {
 } from "@fortawesome/vue-fontawesome";
 
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.component("font-awesome-layers", FontAwesomeLayers);
@@ -24,15 +24,18 @@ Vue.component("font-awesome-layers-text", FontAwesomeLayersText);
 
 Vue.use(Clipboard);
 Vue.use(VueSweetalert2);
-let x = {}
-if (process.env.NODE_ENV === 'production') {
-  console.log('production')
+let x = {};
+if (process.env.NODE_ENV === "production") {
   x = {
     id: "UA-146314933-1",
     router,
-  }
+  };
 }
-Vue.use(VueAnalytics, x);
+
+Vue.use(VueGtag, {
+  config: x,
+});
+// Vue.use(VueAnalytics, x);
 
 // Vue.use(BootstrapVue);
 Vue.use(VAnimateCss);
